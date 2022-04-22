f2e91be1e4d536a0b7c30ceb2486fae3edcb63ea
benjamindraper1996 <benjamin.draper@hotmail.co.uk>
2022-04-22 19:38:01 +0000

    Add random word function and print to console

--- run.py
+++ run.py
@@ -1,2 +1,26 @@
-print('Testing Heroku')
-print('Testing Heroku')
+import random
+
+# List of words for use inside the game.
+LIBRARY = ["computer", "approach", "daughter", "positive", "dedicate",
+           "solution", "evaluate", "minimize", "delicate", "producer",
+           "definite", "hardware", "terminal", "finished", "absolute",
+           "scenario", "argument", "category", "variable", "function"]
+
+
+def pick_word(lib):
+    """
+    Chooses a random word from the library.
+    """
+    word = random.choice(lib).lower()
+
+    print(word)
+
+
+def main():
+    """
+    Run all game functions.
+    """
+    pick_word(LIBRARY)
+
+
+main()
