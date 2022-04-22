1acb195b1ed1cb0d5e51a9a5ebc7170c7e17680d
benjamindraper1996 <benjamin.draper@hotmail.co.uk>
2022-04-22 20:28:28 +0000

    Add game function to take user input and check against the game word

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import random
+import string
 
 # List of words for use inside the game.
 LIBRARY = ["computer", "approach", "daughter", "positive", "dedicate",
@@ -13,7 +14,31 @@ def pick_word(lib):
     """
     word = random.choice(lib).lower()
 
-    print(word)
+    return word
+
+
+def game():
+    """
+    Gather user input and check against the 'word' variable.
+    """
+    game_word = pick_word(LIBRARY)
+    letters = set(game_word)
+    alphabet = set(string.ascii_lowercase)
+    attempts = set()
+
+    while len(letters) > 0:
+        print(f'These are the letters you have used: '.join(attempts))
+        guess = [letter if letter in attempts else '_' for letter in game_word]
+        print(' '.join(guess))
+        user_choice = input('Try and guess a letter:\n').lower()
+        if user_choice in alphabet - attempts:
+            attempts.add(user_choice)
+            if user_choice in letters:
+                letters.remove(user_choice)
+        elif user_choice in attempts:
+            print('This letter has been used, Please try again!')
+        else:
+            print('Invalid character, Please try again!')
 
 
 def main():
@@ -21,6 +46,7 @@ def main():
     Run all game functions.
     """
     pick_word(LIBRARY)
+    game()
 
 
 main()
